c26a1893a1977fe0686b53c65d72e7545ad6ee70
Lucas Pires Nogueira <68610729+iLucasPires@users.noreply.github.com>
2022-09-29 04:58:22 -0300

    Update Main.tsx

--- src/components/organisms/Main.tsx
+++ src/components/organisms/Main.tsx
@@ -4,7 +4,7 @@ export const Main = () => {
   return (
     <main className="flex flex-col overflow-hidden">
       <div className="bg-white flex h-[90vh] w-screen items-center justify-center bg-cover">
-        <h1 className="text-9xl font-black uppercase text-white decoration-current drop-shadow-2xl">
+        <h1 className="text-9xl font-black uppercase text-black decoration-current drop-shadow-2xl">
           Minimalist
         </h1>
       </div>
