ba47bd43aa114a42bd2f40110d9beee8e49ec430
Lucas Pires Nogueira <68610729+iLucasPires@users.noreply.github.com>
2022-09-29 04:53:07 -0300

    color

--- src/components/organisms/Main.tsx
+++ src/components/organisms/Main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Main = () => {
   return (
     <main className="flex flex-col overflow-hidden">
-      <div className="bg-minimalist flex h-[90vh] w-screen items-center justify-center bg-cover">
+      <div className="bg-white flex h-[90vh] w-screen items-center justify-center bg-cover">
         <h1 className="text-9xl font-black uppercase text-white decoration-current drop-shadow-2xl">
           Minimalist
         </h1>
